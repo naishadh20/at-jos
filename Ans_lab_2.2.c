dec2804f933f86903ce570584ab4c4210d3a92ec
k20392 <k20392@users.noreply.github.com>
2016-09-26 09:51:43 -0400

    Update Ans_lab_2.2.c

--- Ans_lab_2.2.c
+++ Ans_lab_2.2.c
@@ -5,6 +5,30 @@
 //Input Arguments: pointer to the page directory, pointer to va, create flag 
 //Returns: Pointer to the page table entry
 
+
+// Given 'pgdir', a pointer to a page directory, pgdir_walk returns
+// a pointer to the page table entry (PTE) for linear address 'va'.
+// This requires walking the two-level page table structure.
+//
+// The relevant page table page might not exist yet.
+// If this is true, and create == false, then pgdir_walk returns NULL.
+// Otherwise, pgdir_walk allocates a new page table page with page_alloc.
+//    - If the allocation fails, pgdir_walk returns NULL.
+//    - Otherwise, the new page's reference count is incremented,
+//	the page is cleared,
+//	and pgdir_walk returns a pointer into the new page table page.
+//
+// Hint 1: you can turn a Page * into the physical address of the
+// page it refers to with page2pa() from kern/pmap.h.
+//
+// Hint 2: the x86 MMU checks permission bits in both the page directory
+// and the page table, so it's safe to leave permissions in the page
+// directory more permissive than strictly necessary.
+//
+// Hint 3: look at inc/mmu.h for useful macros that mainipulate page
+// table and page directory entries.
+//
+
 pte_t *
 pgdir_walk(pde_t *pgdir, const void *va, int create)
 {
@@ -34,9 +58,20 @@ if(!(pgdir[dir_idx] & PTE_P)) //if relevant page table doesn't exist
 }
 
 ////////////////////////////////////////////////////////
-	PDX(va) & PTX(va) ---> memlayout.h		
+	PDX(va) & PTX(va) ---> mmu.h		
 ////////////////////////////////////////////////////////
-
+// A linear address 'la' has a three-part structure as follows:
+//
+// +--------10------+-------10-------+---------12----------+
+// | Page Directory |   Page Table   | Offset within Page  |
+// |      Index     |      Index     |                     |
+// +----------------+----------------+---------------------+
+//  \--- PDX(la) --/ \--- PTX(la) --/ \---- PGOFF(la) ----/
+//  \---------- PGNUM(la) ----------/
+//
+// The PDX, PTX, PGOFF, and PGNUM macros decompose linear addresses as shown.
+// To construct a linear address la from PDX(la), PTX(la), and PGOFF(la),
+// use PGADDR(PDX(la), PTX(la), PGOFF(la)).
 // page number field of address
 #define PGNUM(la)	(((uintptr_t) (la)) >> PTXSHIFT)
 
@@ -79,6 +114,18 @@ _kaddr(const char *file, int line, physaddr_t pa)
 //Input Arguments: pointer to the page directory, pointer to va, size, permission bit 
 //Returns: Pointer to the page table entry
 
+//
+// Map [va, va+size) of virtual address space to physical [pa, pa+size)
+// in the page table rooted at pgdir.  Size is a multiple of PGSIZE, and
+// va and pa are both page-aligned.
+// Use permission bits perm|PTE_P for the entries.
+//
+// This function is only intended to set up the ``static'' mappings
+// above UTOP. As such, it should *not* change the pp_ref field on the
+// mapped pages.
+//
+// Hint: the TA solution uses pgdir_walk
+
 static void
 boot_map_region(pde_t *pgdir, uintptr_t va, size_t size, physaddr_t pa, int perm)
 {
@@ -100,6 +147,19 @@ pa= pa+PGSIZE;
 
 //Input Arguments: pointer to the page directory, pointer to va, size, ** to store page table entry 
 //Returns: page (struct PageInfo)
+
+//
+// Return the page mapped at virtual address 'va'.
+// If pte_store is not zero, then we store in it the address
+// of the pte for this page.  This is used by page_remove and
+// can be used to verify page permissions for syscall arguments,
+// but should not be used by most callers.
+//
+// Return NULL if there is no page mapped at va.
+//
+// Hint: the TA solution uses pgdir_walk and pa2page.
+//
+
 struct PageInfo *
 page_lookup(pde_t *pgdir, void *va, pte_t **pte_store)
 {
@@ -137,6 +197,22 @@ pa2page(physaddr_t pa)
 //Input Arguments: pointer to the page directory, pointer to va, size
 //Returns:void
 
+//
+// Unmaps the physical page at virtual address 'va'.
+// If there is no physical page at that address, silently does nothing.
+//
+// Details:
+//   - The ref count on the physical page should decrement.
+//   - The physical page should be freed if the refcount reaches 0.
+//   - The pg table entry corresponding to 'va' should be set to 0.
+//     (if such a PTE exists)
+//   - The TLB must be invalidated if you remove an entry from
+//     the page table.
+//
+// Hint: The TA solution is implemented using page_lookup,
+// 	tlb_invalidate, and page_decref.
+//
+
 void
 page_remove(pde_t *pgdir, void *va)
 {
@@ -168,7 +244,10 @@ page_decref(struct PageInfo* pp)
 ////////////////////////////////////////////////////////
 	tlb_invalidate() ---> pmap.c		
 ///////////////////////////////////////////////////////
-
+//
+// Invalidate a TLB entry, but only if the page tables being
+// edited are the ones currently in use by the processor.
+//
 void
 tlb_invalidate(pde_t *pgdir, void *va)
 {
@@ -182,6 +261,32 @@ tlb_invalidate(pde_t *pgdir, void *va)
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Input Arguments: pointer to the page directory,page(struct PageInfo),permission
 //Returns: int (success flag)
+
+//
+// Map the physical page 'pp' at virtual address 'va'.
+// The permissions (the low 12 bits) of the page table entry
+// should be set to 'perm|PTE_P'.
+//
+// Requirements
+//   - If there is already a page mapped at 'va', it should be page_remove()d.
+//   - If necessary, on demand, a page table should be allocated and inserted
+//     into 'pgdir'.
+//   - pp->pp_ref should be incremented if the insertion succeeds.
+//   - The TLB must be invalidated if a page was formerly present at 'va'.
+//
+// Corner-case hint: Make sure to consider what happens when the same
+// pp is re-inserted at the same virtual address in the same pgdir.
+// However, try not to distinguish this case in your code, as this
+// frequently leads to subtle bugs; there's an elegant way to handle
+// everything in one code path.
+//
+// RETURNS:
+//   0 on success
+//   -E_NO_MEM, if page table couldn't be allocated
+//
+// Hint: The TA solution is implemented using pgdir_walk, page_remove,
+// and page2pa.
+//
 int
 page_insert(pde_t *pgdir, struct PageInfo *pp, void *va, int perm)
 {
